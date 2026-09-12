3f3edc860976a7e97c8cb472a79b6a72b80b9e20
Shreya-91pandey <sp327272@gmail.com>
2026-09-13 04:34:12 +0530

    Add Color.kt with theme color definitions

---
+++ app/java/com/mahakal/mobilehub/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.mahakal.mobilehub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PurplePrimary = Color(0xFF6B21A8)
+val PinkAccent = Color(0xFFDB2777)
+val CreamBackground = Color(0xFFFFF7ED)
+val DarkText = Color(0xFF1F2937)
+val LightText = Color(0xFFFFFFFF)
+val CardWhite = Color(0xFFFFFFFF)
+val GreenSuccess = Color(0xFF16A34A)
+val OrangeOffer = Color(0xFFEA580C)
